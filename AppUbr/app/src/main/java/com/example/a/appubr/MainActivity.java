84f423c818750aa1d1e159c0f5791a086c653355
kgit1 <kgit1@rambler.ru>
2017-06-05 23:57:46 +0300

    AppUbr button on map

--- AppUbr/app/src/main/java/com/example/a/appubr/MainActivity.java
+++ AppUbr/app/src/main/java/com/example/a/appubr/MainActivity.java
@@ -70,7 +70,3 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-//i-07152ef1be1fd1a3c
-//ec2-54-218-40-215.us-west-2.compute.amazonaws.com
-//3c60334e299611ce00f6e8d2145ff50e1c14b8f2
-//f94308b48971562b7d7be2ff815a0bcdf269cbe7
--- AppUbr/app/src/main/java/com/example/a/appubr/RiderActivity.java
+++ AppUbr/app/src/main/java/com/example/a/appubr/RiderActivity.java
@@ -1,7 +1,18 @@
 package com.example.a.appubr;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +24,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class RiderActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +45,10 @@ public class RiderActivity extends FragmentActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
+
     }
 
 
@@ -38,9 +65,54 @@ public class RiderActivity extends FragmentActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("Location", location.toString());
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                LatLng userLocation = new LatLng(latitude, longitude);
+                mMap.addMarker(new MarkerOptions().position(userLocation).title(latitude + " : " + longitude));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        //if SDK < 23(Marshmallow) - we don't need permission and can ask location immediately
+        if (Build.VERSION.SDK_INT < 23) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }//if SDK > 23
+        else {
+            //check do we have permission, if haven't - ask
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                //ask permission
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }//if have - run code to update location
+            else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+
+        /*// Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
     }
 }
