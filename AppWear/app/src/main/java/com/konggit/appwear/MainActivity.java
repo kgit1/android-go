9b9bae025565f7ff9393bdaf1a8071c2841fac3f
kgit1 <kgit1@rambler.ru>
2017-07-05 23:05:20 +0300

    AppWear start

---
+++ AppWear/app/src/main/java/com/konggit/appwear/MainActivity.java
@@ -0,0 +1,20 @@
+package com.konggit.appwear;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+    }
+}
