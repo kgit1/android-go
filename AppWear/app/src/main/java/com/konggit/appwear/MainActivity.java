e2364d6ee082da3047b2182e6a74c4ffaf087e64
kgit1 <kgit1@rambler.ru>
2017-07-06 00:35:36 +0300

    AppWear round not round

--- AppWear/app/src/main/java/com/konggit/appwear/MainActivity.java
+++ AppWear/app/src/main/java/com/konggit/appwear/MainActivity.java
@@ -3,7 +3,11 @@ package com.konggit.appwear;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -15,6 +19,28 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         mTextView = (TextView) findViewById(R.id.text);
+
+        Button button =(Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(findViewById(R.id.rect_layout)!=null){
+
+                    Log.i("Info","Rectangular layout");
+                    Toast.makeText(getApplicationContext(),"Hello SQUARE!",Toast.LENGTH_SHORT).show();
+
+                }else{
+
+                    Log.i("Info","Round layout");
+                    Toast.makeText(getApplicationContext(),"Hello ROUND!",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
 
     }
 }
