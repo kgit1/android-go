8256f3abbb987101ddb172ed770a87c7be637c69
kgit1 <kgit1@rambler.ru>
2017-07-09 23:59:06 +0300

    AppWearNotifications

---
+++ AppWearNotifications/app/src/main/java/com/konggit/appwearnotifications/MainActivity.java
@@ -0,0 +1,36 @@
+package com.konggit.appwearnotifications;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+        .setSmallIcon(android.R.drawable.arrow_up_float)
+                .setContentTitle("Notification Title")
+                .setContentText("Hi Rob, how's it going")
+                .setContentIntent(pendingIntent);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(0, notificationBuilder.build());
+
+    }
+}
